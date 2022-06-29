2fe63233c5a59687f4115415f99890f9c6a8da66
Michael Asimakopoulos <masimplo@gmail.com>
2022-06-29 03:34:44 +0300

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import '../styles/globals.css';
+import { GoogleAnalytics, usePageViews } from 'nextjs-google-analytics';
 import 'prismjs/themes/prism-tomorrow.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  usePageViews();
+
   return (
     <>
       <span className="theme-bejamas" />
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </>
   );
